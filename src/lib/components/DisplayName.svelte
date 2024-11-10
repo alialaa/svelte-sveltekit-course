9a4fc894337ffd6c721b2694c0ee971deaab4256
alialaa <hello@alialaa.com>
2024-11-10 12:01:20 +0200

    Lecture Code For adding-reactive-values-to-our-simple-component

--- src/lib/components/DisplayName.svelte
+++ src/lib/components/DisplayName.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
-	let name = 'Ali Alaa';
+	let firstName = $state('Ali');
+	let lastName = $state('Alaa');
+	let fullName = $derived.by(() => {
+		return `${firstName} ${lastName}`;
+	});
+	let username = $state('alialaa');
 	let src = 'https://picsum.photos/250/300';
 	let bio = 'Hello, <b>this</b> is me.';
+
+	$effect(() => {
+		if (username || fullName) {
+			console.log('User has a username or a full name.');
+		}
+	});
 </script>
 
-<img {src} alt="A photo of {name}" />
-<h1>{name}</h1>
+<input bind:value={username} />
+<input bind:value={firstName} />
+<input bind:value={lastName} />
+<!-- <img {src} alt="A photo of {name}" /> -->
+<h1>{username || fullName}</h1>
 <p>{@html bio}</p>
 
 <style>
