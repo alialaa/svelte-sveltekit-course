615a46f458317bf1c099f1ffb3d9426381eb9aa6
alialaa <hello@alialaa.com>
2024-11-10 11:47:51 +0200

    Lecture Code For basics-of-a-svelte-component

---
+++ src/lib/components/DisplayName.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let name = 'Ali Alaa';
+	let src = 'https://picsum.photos/250/300';
+	let bio = 'Hello, <b>this</b> is me.';
+</script>
+
+<img {src} alt="A photo of {name}" />
+<h1>{name}</h1>
+<p>{@html bio}</p>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
