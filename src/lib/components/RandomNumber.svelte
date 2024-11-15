3be881da1de647db01824ad68bc2d645fafe975d
alialaa <hello@alialaa.com>
2024-11-15 10:32:37 +0200

    Lecture Code For avoid-effects-to-sync-state

---
+++ src/lib/components/RandomNumber.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let randomNumber = $state(Math.floor(Math.random() * 10));
+	// let doubleRandomNumber = $state();
+	let doubleRandomNumber = $derived(randomNumber * 2);
+
+	// $effect(() => {
+	// 	doubleRandomNumber = randomNumber * 2;
+	// });
+</script>
+
+<h2>The random number is: {randomNumber}</h2>
+<h2>Double random number is: {doubleRandomNumber}</h2>
+
+<button
+	onclick={() => {
+		randomNumber = Math.floor(Math.random() * 10);
+		console.log({ randomNumber, doubleRandomNumber });
+	}}>Generate</button
+>
