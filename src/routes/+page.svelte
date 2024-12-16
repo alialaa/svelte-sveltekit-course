015647de281ce89789a4911939ddda4141b2788d
alialaa <hello@alialaa.com>
2024-12-16 14:04:07 +0200

    Lecture Code For raw-state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
+	import Button from '$lib/components/Button.svelte';
 	import Notification from '$lib/components/Notification.svelte';
 	import generateNotifications from '$lib/utils/generate-notifications';
 
-	let notifications = $state(generateNotifications(3));
-
-	$inspect(notifications);
+	let notifications = $state.raw(generateNotifications(3));
 </script>
 
+<Button
+	onclick={() => {
+		notifications = generateNotifications(3);
+	}}>Refresh</Button
+>
 <ul>
 	{#each notifications as notification, index (notification.id)}
 		<li>
 			<Notification
 				{notification}
 				onremove={(id) => {
-					notifications.splice(index, 1);
+					notifications = notifications.filter((n) => n.id !== id);
 				}}
 			/>
 		</li>
