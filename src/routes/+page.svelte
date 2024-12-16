e7a3ead391021663501396db09520760648fc641
alialaa <hello@alialaa.com>
2024-12-16 11:57:40 +0200

    Lecture Code For deep-state-intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,55 @@
 <script lang="ts">
-	const target = {
+	let array = $state([1, 2, 3, 4]);
+	let objectsArray = $state([{ id: 1 }, { id: 2 }]);
+	let object = $state({
 		firstName: 'Ali',
 		lastName: 'Alaa',
-		occupations: [],
-		get fullName() {
-			return `${this.firstName} ${this.lastName}`;
-		},
-		set occupation(value: string) {
-			console.log(`Adding ${value} to occupations`);
-			this.occupations.push(value);
+		address: {
+			city: 'City',
+			street: 'Street'
 		}
-	};
+	});
 
-	const handler = {
-		get(target, prop) {
-			// console.log(target, prop);
-			return prop in target ? target[prop] : 'NA';
-		},
-		set(target, prop, value) {
-			if (['firstName', 'lastName'].includes(prop)) {
-				if (typeof value !== 'string') {
-					throw new TypeError(`Property ${prop} must be a string.`);
-				}
-			}
-			target[prop] = value;
-			return true;
-		}
-	};
-	const proxy = new Proxy(target, handler);
-	proxy.firstName = 'Omar';
-	console.log(proxy.firstName);
+	$effect(() => {
+		console.log('object effect');
+		console.log(object);
+	});
+	$effect(() => {
+		console.log('object.firstName effect');
+		console.log(object.firstName);
+	});
+	$effect(() => {
+		console.log('object.address.city');
+		console.log(object.address.city);
+	});
+	$effect(() => {
+		console.log('array');
+		console.log(array[0]);
+	});
+	$effect(() => {
+		console.log('array.length');
+		console.log(array.length);
+	});
 </script>
+
+<h2>{object.firstName}</h2>
+<h2>{object.address.city}</h2>
+
+<input bind:value={object.firstName} />
+<input bind:value={object.address.city} />
+<input bind:value={object.address.street} />
+<p>{array}</p>
+<button
+	onclick={() => {
+		array[1] = Math.random() * 10;
+	}}>Add to array</button
+>
 
 <style>
 	:global {
 		body {
 			background-color: #222;
+			color: #fff;
 		}
 	}
 </style>
