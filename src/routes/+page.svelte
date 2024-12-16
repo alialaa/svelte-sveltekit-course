8cf6d1e3decd06e2342f8cd210f2fa01e0407432
alialaa <hello@alialaa.com>
2024-12-16 11:03:29 +0200

    Lecture Code For proxies-concept

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,65 +1,41 @@
 <script lang="ts">
-	import Button from '$lib/components/Button.svelte';
-	import { AlarmCheck, Search } from 'lucide-svelte';
-	let html = '<p>dwdwdw</p>';
+	const target = {
+		firstName: 'Ali',
+		lastName: 'Alaa',
+		occupations: [],
+		get fullName() {
+			return `${this.firstName} ${this.lastName}`;
+		},
+		set occupation(value: string) {
+			console.log(`Adding ${value} to occupations`);
+			this.occupations.push(value);
+		}
+	};
 
-	let button: Button;
-
-	$effect(() => {
-		button.getButton().focus();
-		// or
-		// button.focus()
-	});
+	const handler = {
+		get(target, prop) {
+			// console.log(target, prop);
+			return prop in target ? target[prop] : 'NA';
+		},
+		set(target, prop, value) {
+			if (['firstName', 'lastName'].includes(prop)) {
+				if (typeof value !== 'string') {
+					throw new TypeError(`Property ${prop} must be a string.`);
+				}
+			}
+			target[prop] = value;
+			return true;
+		}
+	};
+	const proxy = new Proxy(target, handler);
+	proxy.firstName = 'Omar';
+	console.log(proxy.firstName);
 </script>
-
-<div class="wrapper">
-	{@html html}
-	<div
-		role="presentation"
-		onclick={(e) => {
-			e.stopPropagation();
-			console.log('event coming from div');
-		}}
-	>
-		<Button
-			bind:this={button}
-			href={undefined}
-			size="lg"
-			--buttonBgColor="yellow"
-			--buttonTextColor="green"
-			onclick={(e) => {
-				e.stopPropagation();
-				console.log('event coming from button');
-			}}
-			onlefthover={() => {
-				console.log('left hovered');
-			}}
-		>
-			{#snippet left(isHovered)}
-				{#if isHovered}
-					<Search />
-				{:else}
-					<AlarmCheck />
-				{/if}
-			{/snippet}
-			Text
-			{#snippet right()}
-				<AlarmCheck />
-			{/snippet}
-		</Button>
-	</div>
-</div>
 
 <style>
 	:global {
 		body {
 			background-color: #222;
-		}
-	}
-
-	.wrapper :global {
-		p {
-			color: white;
 		}
 	}
 </style>
