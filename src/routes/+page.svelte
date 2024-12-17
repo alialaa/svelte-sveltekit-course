27c2ea955aed7b4b0b1b346776a5f05b6673c9fc
alialaa <hello@alialaa.com>
2024-12-17 09:35:20 +0200

    Lecture Code For sheet-component-init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,14 @@
 <script lang="ts">
-	import Button from '$lib/components/Button.svelte';
-	import Notification from '$lib/components/Notification.svelte';
-	import generateNotifications from '$lib/utils/generate-notifications';
-
-	let notifications = $state.raw(generateNotifications(3));
+	import Sheet from '$lib/components/sheet/Sheet.svelte';
 </script>
 
-<Button
-	onclick={() => {
-		notifications = generateNotifications(3);
-	}}>Refresh</Button
->
-<ul>
-	{#each notifications as notification, index (notification.id)}
-		<li>
-			<Notification
-				{notification}
-				onremove={(id) => {
-					notifications = notifications.filter((n) => n.id !== id);
-				}}
-			/>
-		</li>
-	{:else}
-		<p>No notifications</p>
-	{/each}
-</ul>
+<Sheet />
 
 <style>
 	:global {
 		body {
 			background-color: #222;
 			color: #fff;
-		}
-	}
-
-	ul {
-		list-style: none;
-		padding: 10px;
-		margin: 0;
-		li {
-			margin-bottom: 10px;
 		}
 	}
 </style>
