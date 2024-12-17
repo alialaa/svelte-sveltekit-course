f6de97b42275199db348d4520679e5e006abb24d
alialaa <hello@alialaa.com>
2024-12-17 11:25:21 +0200

    Lecture Code For sheet-component-edit-cell-functionality

--- src/lib/components/sheet/Sheet.svelte
+++ src/lib/components/sheet/Sheet.svelte
@@ -17,6 +17,19 @@
 		el.focus();
 		el.select();
 	}
+
+	function setCell(row: number, col: number, prop: 'value' | 'bgColor' | 'color', value: string) {
+		if (data[row]) {
+			if (data[row][col]) {
+				data[row][col][prop] = value;
+			} else {
+				data[row][col] = { [prop]: value };
+			}
+		} else {
+			data[row] = [];
+			data[row][col] = { [prop]: value };
+		}
+	}
 </script>
 
 <table class="sheet">
@@ -61,7 +74,7 @@
 									style:background-color={cellData?.bgColor}
 									style:color={cellData?.color}
 									oninput={(e) => {
-										console.log(e.currentTarget.value);
+										setCell(row - 1, column - 1, 'value', e.currentTarget.value);
 									}}
 								/>
 							{/if}
