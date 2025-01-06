90b42584d604172b929fb0be7005b3fb986069d4
alialaa <hello@alialaa.com>
2025-01-06 12:49:02 +0200

    Lecture Code For currency-converter-reactive-class-4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import CurrencyConverter from '$lib/components/CurrencyConverter.svelte';
+
+	import CC from '$lib/utils/currency-converter.svelte';
+
+	const cc = new CC(10, 'gbp', 'usd');
 </script>
 
+{cc.rate}
+{cc.baseCurrency}
+{cc.baseValue}
+{cc.targetValue}
 <CurrencyConverter />
 
 <style>
