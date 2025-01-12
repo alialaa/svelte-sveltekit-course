c0aae49c07a897aa8bbd587cebff3409fc0171e4
alialaa <hello@alialaa.com>
2025-01-12 12:44:44 +0200

    Lecture Code For createSubscriber-intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,30 @@
 <script lang="ts">
-	import Button from '$lib/components/Button.svelte';
-	import ClickToCount from '$lib/components/ClickToCount.svelte';
-	import count, { increment } from '$lib/utils/counter.svelte';
+	import { browser } from '$app/environment';
+	import { on } from 'svelte/events';
+	import { createSubscriber } from 'svelte/reactivity';
+
+	let show1 = $state(true);
+	let show2 = $state(true);
+
+	const sub = createSubscriber((update) => {
+		console.log('init');
+		const off = on(window, 'scroll', update);
+		return () => {
+			console.log('cleanup');
+			off();
+		};
+	});
 </script>
 
-<Button onclick={increment}>Increment</Button>
-{count.value}
-
-<ClickToCount />
-<ClickToCount />
-<ClickToCount />
+<input type="checkbox" bind:checked={show1} />
+<input type="checkbox" bind:checked={show2} />
+{#if show1}
+	<h1 style="position: fixed;">{sub()}{browser ? window.scrollY : 0}</h1>
+{/if}
+{#if show2}
+	<h1 style="position: fixed;top: 100px">{sub()}{browser ? window.scrollY : 0}</h1>
+{/if}
+<div style="height: 2000px;"></div>
 
 <style>
 	:global {
