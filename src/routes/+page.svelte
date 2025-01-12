347fb4db95e7999d1eca546f761ce4d04b1dc7d6
alialaa <hello@alialaa.com>
2025-01-12 13:12:42 +0200

    Lecture Code For createSubscriber-in-classes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,19 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
-	import { on } from 'svelte/events';
-	import { createSubscriber } from 'svelte/reactivity';
+	// import scrollY from '$lib/utils/scroll-y.svelte';
+	import { scrollY } from 'svelte/reactivity/window';
 
 	let show1 = $state(true);
 	let show2 = $state(true);
-
-	const sub = createSubscriber((update) => {
-		console.log('init');
-		const off = on(window, 'scroll', update);
-		return () => {
-			console.log('cleanup');
-			off();
-		};
-	});
 </script>
 
 <input type="checkbox" bind:checked={show1} />
 <input type="checkbox" bind:checked={show2} />
 {#if show1}
-	<h1 style="position: fixed;">{sub()}{browser ? window.scrollY : 0}</h1>
+	<h1 style="position: fixed;">{scrollY.current}</h1>
 {/if}
 {#if show2}
-	<h1 style="position: fixed;top: 100px">{sub()}{browser ? window.scrollY : 0}</h1>
+	<h1 style="position: fixed;top: 100px">{scrollY.current}</h1>
 {/if}
 <div style="height: 2000px;"></div>
 
