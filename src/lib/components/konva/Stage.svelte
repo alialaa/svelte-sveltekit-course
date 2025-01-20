ecc15f4ec0862246b0aa6a1ccfbf2aa905a1ebc4
alialaa <hello@alialaa.com>
2025-01-20 12:25:25 +0200

    Lecture Code For konva-stage

---
+++ src/lib/components/konva/Stage.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Konva from 'konva';
+	import type { Snippet } from 'svelte';
+
+	let { children, ...props }: { children?: Snippet } & Konva.StageConfig = $props();
+	let container: HTMLDivElement;
+	let stage: Konva.Stage;
+
+	$effect(() => {
+		stage = new Konva.Stage({
+			container,
+			...props
+		});
+		return () => {
+			stage.destroy();
+		};
+	});
+</script>
+
+<div bind:this={container}>
+	{@render children?.()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,8 @@
 <script lang="ts">
-	import Konva from 'konva';
-
-	let container: HTMLDivElement;
-
-	$effect(() => {
-		const stage = new Konva.Stage({
-			container,
-			width: 500,
-			height: 500
-		});
-		const layer = new Konva.Layer();
-		const rect1 = new Konva.Rect({
-			x: 20,
-			y: 20,
-			width: 100,
-			height: 60,
-			fill: 'purple',
-			stroke: 'white',
-			strokeWidth: 4
-		});
-		layer.add(rect1);
-		stage.add(layer);
-	});
+	import { Stage } from '$lib/components/konva';
 </script>
 
-<div bind:this={container}></div>
+<Stage width={500} height={500}></Stage>
 
 <style>
 	:global {
