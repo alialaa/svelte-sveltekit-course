94cceb180a0b43728da691df68dd1af8115d7306
alialaa <hello@alialaa.com>
2025-01-27 18:10:03 +0200

    Lecture Code For video-bindings

---
+++ src/lib/components/VideoPlayer.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	let { src }: { src: string } = $props();
+	let duration = $state(0);
+	let currentTime = $state(0);
+	let paused = $state(true);
+	let seeking = $state(false);
+	let playbackRate = $state(1);
+	let volume = $state(1);
+	let buffered = $state([{ start: 0, end: 0 }]);
+</script>
+
+<!-- svelte-ignore a11y_media_has_caption -->
+<video
+	{src}
+	bind:duration
+	bind:currentTime
+	bind:paused
+	bind:seeking
+	bind:playbackRate
+	bind:volume
+	bind:buffered
+></video>
+
+<ul>
+	<li>duration: {duration}</li>
+	<li>currentTime: {currentTime}</li>
+	<li>paused: {paused}</li>
+	<li>seeking: {seeking}</li>
+	<li>playbackRate: {playbackRate}</li>
+	<li>volume: {volume}</li>
+	<li>buffered: {JSON.stringify(buffered)}</li>
+</ul>
+
+<style lang="scss">
+	video {
+		max-width: 100%;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	let textAreaWidth = $state();
-	let textAreaHeight = $state();
+	import VideoPlayer from '$lib/components/VideoPlayer.svelte';
 </script>
 
-{textAreaWidth}x{textAreaHeight}
-<textarea bind:offsetWidth={textAreaWidth} bind:offsetHeight={textAreaHeight}></textarea>
+<VideoPlayer
+	src="https://previews.customer.envatousercontent.com/c6376000-ac71-4351-b229-1e2f1763034b/watermarked_preview/watermarked_preview.mp4"
+/>
 
 <style>
 	:global {
